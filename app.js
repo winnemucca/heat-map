eeb55f653b6c57eddaaba306be66fe586b14493f
Steven Kauyedauty <skauyedauty@gmail.com>
2015-09-23 04:17:00 -0600

    added start place

--- app.js
+++ app.js
@@ -33,3 +33,63 @@
 // 	}
 // 	
 // 	})()
+// 	
+// 	
+// 	
+// 	
+// 	// $('.jumbotron').hover(function(evt) {
+// 	  enteredTime = new Date();
+// 	}, function() {
+// 	  var ctime = new Date();
+// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
+
+// 	  jumbotronArray.push(time);
+// 	  console.log('this', $(this));
+// 	  // console.log('added',Math.round(jumbotronArray.reduce(add,0) ));
+// 	})
+// 	console.log('jumbo array', jumbotronArray);
+// 	$('.panel-heading').hover(function(evt) {
+// 	  enteredTime = new Date();
+// 	  // console.log('e',enteredTime)
+// 	}, function() {
+// 	  var ctime = new Date();
+// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
+
+// 	  panelHeadingArray.push(time);
+
+// 	})
+// 	$('.panel-body').hover(function(evt) {
+// 	  enteredTime = new Date();
+// 	}, function() {
+// 	  var ctime = new Date();
+// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
+
+// 	  panelBodyArray.push(time);
+	
+// 	})
+
+// 	$('.list-group').hover(function(evt) {
+// 	  enteredTime = new Date();
+// 	}, function() {
+// 	  var ctime = new Date();
+// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
+
+// 	  listGroupArray.push(time);
+	 
+// 	})
+// 	
+// 	
+// 	
+// 	**************************
+// 	
+	// ideal refactor mode.  Right now cannot individual push time into own arrays//
+	
+	// $('.jumbotron').bind('mouseenter mouseleave', timeMonitering.inAndOut);
+
+	// $('.navbar').bind('mouseenter mouseleave', timeMonitering.inAndOut);
+
+	// $('.panel-heading').bind('mouseenter mouseleave', timeMonitering.inAndOut);
+
+	// $('.panel-body').bind('mouseenter mouseleave', timeMonitering.inAndOut);
+
+	// $('.list-group').bind('mouseenter mouseleave', timeMonitering.inAndOut);
--- main.js
+++ main.js
@@ -1,53 +1,26 @@
 $(document).on('ready', function() {
+	// overall timer
 	var timeCount = 0;
 	var countToSignUp;
 	var signedUp = false;
 	var intervalID;
-	var scrollRangeList = [];
+	// var distanceScrolled = 0;
+	var distanceScrolled = [];
+	var userStart;
+
 	var windowHeight = $(window).height();
-
-	var timer = function(command) {
-		if(command === 'start') {
-
-			intervalID = setInterval(function() {timeCount++; heatmapSectionCounter(); }, 1000);
-		}
-		else {
-			clearInterval(intervalID);
-		}
-	}
-
-	timer("start");
-
-
-	setTimeout(function(){
-		window.bodyHeight = $("body").height();
-		console.log('body height',window.bodyHeight)
-		window.heatmapSectionTotal = Math.ceil(bodyHeight/300);
-		// console.log('section',window.heatmapSectionTotal)
-		window.heatmapSectionRanges = [];
-		for (var i = 0; i < heatmapSectionTotal; i++) {
-			heatmapSectionRanges.push([windowHeight * i, windowHeight * i + windowHeight, 0]);
-			};
-		window.heatmapSectionCounter = function() {
-		for (var i = 0; i < heatmapSectionTotal; i++) {
-			if($("body").scrollTop() < heatmapSectionRanges[i][1]  && $("body").scrollTop() > heatmapSectionRanges[i][0]) {
-				heatmapSectionRanges[i][2]++;
-				}
-			}
-		}
-		console.log('ranges',window.heatmapSectionRanges)
-	}, 500);
-
-	$(document).on('scroll', function(){
-		scrollRangeList.push([$('body').scrollTop(), $('body').scrollTop() + windowHeight]);
-		// console.log('list',scrollRangeList);
-		
-	});
-
-
-	// end 
-	
+	// div time
 	var enteredTime = 0;
+	// var off =  dw_getScrollOffsets();
+	var doc = document.documentElement;
+	var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+	var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+	console.log( top);
+
+
+	var start = function() {
+
+	}
 	
 	var jumbotronHeight = $('.jumbotron').height();
 	var panelHeadingHeight = $('.panel-heading').height();
@@ -77,17 +50,69 @@ $(document).on('ready', function() {
 	    }
 	}
 
+	var timer = function(command) {
+		if(command === 'start') {
+
+			intervalID = setInterval(function() {timeCount++; heatmapSectionCounter(); }, 1000);
+		}
+		else {
+			clearInterval(intervalID);
+		}
+	}
+	timer("start");
+
+
+	setTimeout(function(){
+		// body height
+		window.bodyHeight = $("body").height();
+		console.log(window.bodyHeight)
+		window.heatmapSectionTotal = Math.ceil(bodyHeight/300);
+		console.log('section',window.heatmapSectionTotal)
+		window.heatmapSectionRanges = [];
+		for (var i = 0; i < heatmapSectionTotal; i++) {
+			heatmapSectionRanges.push([windowHeight * i, windowHeight * i + windowHeight, 0]);
+			};
+		window.heatmapSectionCounter = function() {
+		for (var i = 0; i < heatmapSectionTotal; i++) {
+			if($("body").scrollTop() < heatmapSectionRanges[i][1]  && $("body").scrollTop() > heatmapSectionRanges[i][0]) {
+				heatmapSectionRanges[i][2]++;
+				}
+			}
+		}
+
+		console.log('ranges',window.heatmapSectionRanges)
+	}, 500);
+
+
+	// function dw_getScrollOffsets() {
+	//     var doc = document, w = window;
+	//     var x, y, docEl;
+	    
+	//     if ( typeof w.pageYOffset === 'number' ) {
+	//         x = w.pageXOffset;
+	//         y = w.pageYOffset;
+	//     } else {
+	//         docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat')?
+	//                 doc.documentElement: doc.body;
+	//         x = docEl.scrollLeft;
+	//         y = docEl.scrollTop;
+	//     }
+	//     return {x:x, y:y};
+	// }
+	// var start;
+	// console.log(off.y)
+	
+
+
 // Object.keys returns an array of a given objects own enumerable properties
 	Object.keys(mapping).forEach(function(selector) {	
 	  $(selector).hover(function(evt) {
-	  	console.log('mapping',mapping);
-		console.log('selector',selector);
 	    enteredTime = new Date();
 	  }, function() {
 	    var ctime = new Date();
 	    var time = (ctime.getTime() - enteredTime.getTime())/1000;
 	    mapping[selector].times.push(time);
-	    mapping[selector].total = mapping[selector].times.reduce(add, 0);
+	    mapping[selector].total = Math.round(mapping[selector].times.reduce(add, 0) );
 	    
 	  	});
 	})
@@ -96,68 +121,28 @@ $(document).on('ready', function() {
 			return a +b;
 	}
 
-	// $('.stats-button').on('click', function() {
-	// 	var total = Math.round(jumbotronArray.reduce(add,0) )
-	// 	$('.jumbotronTotal').empty().append(total);
-	// })
+	$('footer').on('mouseover', function() {
+		var height=window.bodyHeight - top;
+		distanceScrolled.push(height);
+		userStart = parseInt(distanceScrolled[0]);
+		console.log(userStart);
+
+
+	})
+
+	$('.stats-button').on('click', function() {
+		$('.jumbotronTotal').empty().append(mapping[".jumbotron"].total);
+		$('.navbarTotal').empty().append(mapping[".navbar"].total);
+		$('.list').empty().append(mapping[".list-group"].total);
+		$('.panelHead').empty().append(mapping[".panel-heading"].total);
+		$('.panelBody').empty().append(mapping[".panel-body"].total);
+		$('.timeCount').empty().append(timeCount);
+		$('.distanceTotal').empty().append(userStart)
+
+
+	})
 
 });
 
 
 
-// $('.jumbotron').hover(function(evt) {
-// 	  enteredTime = new Date();
-// 	}, function() {
-// 	  var ctime = new Date();
-// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
-
-// 	  jumbotronArray.push(time);
-// 	  console.log('this', $(this));
-// 	  // console.log('added',Math.round(jumbotronArray.reduce(add,0) ));
-// 	})
-// 	console.log('jumbo array', jumbotronArray);
-// 	$('.panel-heading').hover(function(evt) {
-// 	  enteredTime = new Date();
-// 	  // console.log('e',enteredTime)
-// 	}, function() {
-// 	  var ctime = new Date();
-// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
-
-// 	  panelHeadingArray.push(time);
-
-// 	})
-// 	$('.panel-body').hover(function(evt) {
-// 	  enteredTime = new Date();
-// 	}, function() {
-// 	  var ctime = new Date();
-// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
-
-// 	  panelBodyArray.push(time);
-	
-// 	})
-
-// 	$('.list-group').hover(function(evt) {
-// 	  enteredTime = new Date();
-// 	}, function() {
-// 	  var ctime = new Date();
-// 	  var time = (ctime.getTime() - enteredTime.getTime())/1000;
-
-// 	  listGroupArray.push(time);
-	 
-// 	})
-// 	
-// 	
-// 	
-// 	**************************
-// 	
-	// ideal refactor mode.  Right now cannot individual push time into own arrays//
-	
-	// $('.jumbotron').bind('mouseenter mouseleave', timeMonitering.inAndOut);
-
-	// $('.navbar').bind('mouseenter mouseleave', timeMonitering.inAndOut);
-
-	// $('.panel-heading').bind('mouseenter mouseleave', timeMonitering.inAndOut);
-
-	// $('.panel-body').bind('mouseenter mouseleave', timeMonitering.inAndOut);
-
-	// $('.list-group').bind('mouseenter mouseleave', timeMonitering.inAndOut);
